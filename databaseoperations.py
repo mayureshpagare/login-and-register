f4079ef469089feb85549ef5c182612ef624e0bd
Albert Felix <albertfelixleo@gmail.com>
2021-10-18 18:38:12 +0530

    table name updated

--- databaseoperations.py
+++ databaseoperations.py
@@ -11,7 +11,7 @@ def insert_data(userid, password):
     # This function adds new data as ID and Password to the database
     connection = psycopg2.connect(dbname="login", host="localhost", user="postgres", password="mayuresh", port="5432")
     cursor = connection.cursor()
-    insert_query = '''INSERT INTO a_table (userid, password) VALUES(%s, %s)'''
+    insert_query = '''INSERT INTO logindata (userid, password) VALUES(%s, %s)'''
     cursor.execute(insert_query, (userid, password))
     connection.commit()
     cursor.close()

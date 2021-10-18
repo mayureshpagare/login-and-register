813a039ed23a87f22f31da664b5f1fa5b6f9ec7c
Tejas Borde <72294338+tejasborde@users.noreply.github.com>
2021-10-18 17:54:55 +0530

    Update databaseoperations.py

--- databaseoperations.py
+++ databaseoperations.py
@@ -10,6 +10,12 @@
     
     # This function adds new data as ID and Password to the database
 
-# def searchdata(userid, password):
-    
-    # This function checks if the given Id and password is in the database
+def searchdata(userid, password):
+    connection = psycopg2.connect(dbname="login", host="localhost", user="postgres", password="mayuresh", port="5432")
+    cursor = connection.cursor()
+    searchquery = '''SELECT * from logindata where userid= %s and password= %s'''
+    cursor.execute(searchquery, (userid, password))
+    row = cursor.fetchone()
+    connection.commit()
+    connection.close()
+    return row
